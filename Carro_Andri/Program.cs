374d0d67045a79603e077a314376c633a490e0e4
Andri <99926697+andrilucero@users.noreply.github.com>
2022-03-27 22:29:05 -0400

    Add files via upload

---
+++ Carro_Andri/Program.cs
@@ -0,0 +1,29 @@
+﻿Carro miCarrito = new Carro("Ford", 2019, "azul", 120);
+Carro CarritodeAndri = new Carro("Lamborguini", 2022, "Rojo", 150);
+
+
+
+if (miCarrito.Modelo > CarritodeAndri.Modelo)
+{
+    Console.WriteLine("El carro de mi cuate es mas reciente");
+}
+else
+{
+    Console.WriteLine("El carro de Andri es mas reciente");
+}
+
+
+Console.WriteLine($"Andri's car is {CarritodeAndri.Marca}");
+
+Console.WriteLine(CarritodeAndri.GetVelocidadActual());
+Console.WriteLine(miCarrito.DameInformacion());
+CarritodeAndri.EncenderMotor();
+
+for (int i = 0; i < 15; i++)
+{
+    CarritodeAndri.acelerar();
+}
+
+CarritodeAndri.acelerar(2);
+CarritodeAndri.acelerar(4);
+CarritodeAndri.acelerar(1);
---
+++ Carro_Andri/carro.cs
@@ -0,0 +1,123 @@
+﻿internal class Carro
+{
+    public string Marca { get; }
+    public int Modelo { get; }
+
+    public String Color { get; set; }
+    public string Frenar { get; }
+
+    private int Encendido = 0;
+    private int velocidad_actual = 0;
+    public int MAXVELOCIDAD { get;}
+
+
+
+
+
+
+    public Carro(string _marca, int _modelo, string _color, int velocidmax)
+    {
+        Marca = _marca;
+        Modelo = _modelo;
+        Color = _color;
+        Encendido = 0;
+        MAXVELOCIDAD = velocidmax;
+        this.velocidad_actual = 0;
+    }
+
+
+
+    public string acelerar()
+    {
+        if (Encendido == 0)
+        {
+            return $"El carro esta apagado, asi no se puede";
+        }
+
+
+        string mensaje = "";
+        if (velocidad_actual <= MAXVELOCIDAD)
+        {
+            velocidad_actual += 5;
+            mensaje = $"vas a {velocidad_actual} KPH";
+        }
+        else
+        {
+            velocidad_actual = MAXVELOCIDAD;
+            mensaje = "Hey que ondas, vas a maxima velocidad, no te creas Vin Diesel!";
+        }
+
+        Console.WriteLine(mensaje);
+        return mensaje;
+
+    }
+
+
+
+    public string acelerar(int AcelerarAkph)
+    {
+        if (Encendido == 0)
+        {
+            return $"El carro esta apagado, asi no se puede";
+        }
+
+
+        string mensaje = "";
+        if (velocidad_actual <= MAXVELOCIDAD)
+        {
+            velocidad_actual += AcelerarAkph;
+            mensaje = $"vas a {velocidad_actual} KPH";
+        }
+        else
+        {
+            velocidad_actual = MAXVELOCIDAD;
+            mensaje = "Relajese mijo se va accidentar!";
+        }
+
+        Console.WriteLine(mensaje);
+        return mensaje;
+
+    }
+
+    public void freno(int frenar)
+
+    {
+        if (velocidad_actual == 0)
+
+            Console.WriteLine("Que ondas frenaste el carro?");
+
+
+    }
+
+    public string GetVelocidadActual()
+    {
+        return $"Vamos a {velocidad_actual}KPH";
+    }
+
+
+    public void EncenderMotor()
+    {
+        if (Encendido == 0)
+        {
+            Encendido = 1;
+            Console.WriteLine("Brururururururu carro encendido!!");
+            velocidad_actual = 0;
+
+        }
+        else
+        {
+            Console.WriteLine("y que ondas? el carro ya estaba encendido");
+        }
+    }
+
+
+
+
+
+
+    public string DameInformacion()
+    {
+        return ($"Soy un carro marca {Marca} y mi modelo es {Modelo}");
+    }
+
+}
